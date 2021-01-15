8aa28deb572dd61fc33f864512d68beaafe55508
dr497 <anand.ratiney@gmail.com>
2021-01-15 17:38:43 +0800

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage';
 import { useSnackbar } from 'notistack';
 import SnackbarUtils from './utils/SnackbarUtils';
 import NavigationFrame from './components/NavigationFrame';
+import PoolPage from './pages/PoolPage';
+import FaqPage from './pages/FaqPage';
 
 export default function Routes() {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
@@ -15,6 +17,8 @@ export default function Routes() {
       <NavigationFrame>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/faq" component={FaqPage} />
+          <Route exact path="/pool/:poolAddress" component={PoolPage} />
         </Switch>
       </NavigationFrame>
     </BrowserRouter>
